4fef392ac7b00c31fcf61a4f9e05ac230f9be970
gokul-r-7 <113924021+gokul-r-7@users.noreply.github.com>
2024-11-03 19:07:33 +0530

    Update Read_Athena_PySpark.py

--- ETL Athena table to Athena Table/Read_Athena_PySpark.py
+++ ETL Athena table to Athena Table/Read_Athena_PySpark.py
@@ -14,32 +14,58 @@ spark = glueContext.spark_session
 job = Job(glueContext)
 job.init(args['JOB_NAME'], args)
 
-sql_query = 'SELECT * FROM "AwsDataCatalog"."sample-datebase"."test_folder_1"'
+#SQL Queries
+sample_query = 'SELECT * FROM "AwsDataCatalog"."sample-datebase"."test_folder_1"'
+account_dim_sum = ""
+account_dim_sum = ""
+transaction_adobe_fact = ""
+transaction_okta_user_agg_fact = ""
+transcation_okta_day_agg = ""
 
-athena_table_dataframe = (
+# Athena Query Results Temporary files s3 path
+sample_temp_path = "s3://gokul-test-bucket-07/temporary_files/"
+account_dim_sum_temp = ""
+account_dim_sum_temp = ""
+transaction_adobe_fact_temp = ""
+transaction_okta_user_agg_fact_temp = ""
+transcation_okta_day_agg_temp = ""
+
+#Output s3 path
+sample_output_path = "s3://gokul-test-bucket-07/output_files/"
+account_dim_sum_output = ""
+account_dim_sum_output = ""
+transaction_adobe_fact_output = ""
+transaction_okta_user_agg_fact_output = ""
+transcation_okta_day_agg_output = ""
+
+#PartitionKeys for the target files
+sample_partition_keys = ["index"]
+account_dim_sum_partitionkeys = []
+account_dim_sum_partitionkeys = []
+transaction_adobe_fact_partitionkeys = []
+transaction_okta_user_agg_fact_partitionkeys = []
+transcation_okta_day_agg_partitionkeys = []
+
+def read_athena_tables(sql_query,temp_path):
+    read_df = (
     glueContext.read.format("jdbc")
     .option("driver", "com.simba.athena.jdbc.Driver")
     .option("AwsCredentialsProviderClass","com.simba.athena.amazonaws.auth.InstanceProfileCredentialsProvider")
     .option("url", "jdbc:awsathena://athena.us-east-1.amazonaws.com:443")
     .option("dbtable", f"({sql_query})")
-    .option("S3OutputLocation","s3://gokul-test-bucket-07/demo-test/")
+    .option("S3OutputLocation",temp_path)
     .load()
     )
-
-athena_table_dataframe.printSchema()
-athena_table_dataframe.show()
-athena_table_datasource = DynamicFrame.fromDF(athena_table_dataframe, glueContext, "athena_table_source")
-
-# Specify S3 output path
-output_path = "s3://gokul-test-bucket-07/final-target-folder/"
-
-# Write the DynamicFrame to S3 as partitioned CSV files
-csv_output = glueContext.write_dynamic_frame.from_options(
-    frame=athena_table_datasource,
+    return read_df
+    
+def write_csv_files_into_s3(df,output_path,partitionkey):
+    write_df = DynamicFrame.fromDF(df, glueContext, "athena_table_source")
+    csv_output = glueContext.write_dynamic_frame.from_options(
+    frame=write_df,
     connection_type="s3",
     connection_options={
         "path": output_path,
-        "partitionKeys": ["index"]
+        "partitionKeys": partitionkey
     },
     format="csv",
     format_options={
@@ -48,7 +74,19 @@ csv_output = glueContext.write_dynamic_frame.from_options(
         "withHeader": True,
         "escapeChar": "\\" 
     }
-)
+    )
+    return csv_output
+
+
+sample_read_df = read_athena_tables(sample_query,sample_temp_path)
+sample_read_df.printSchema()
+sample_read_df.show()
+sample_read_df.count()
+
+sample_write_df = write_csv_files_into_s3(sample_read_df,sample_output_path,sample_partition_keys)
+sample_write_df.printSchema()
+sample_write_df.show()
+sample_write_df.count()
 
 
 job.commit()
